dce5b9d9ba858d591bbb482246a53100d4f4e56e
OwenMenzies <78829256+OwenMenzies@users.noreply.github.com>
2021-03-10 12:51:49 +1300

    t-flip flow

--- main.ts
+++ main.ts
@@ -1,26 +1,57 @@
 let moistness = 0
-input.onButtonPressed(Button.A, function () {
-    basic.showNumber(pins.analogReadPin(AnalogPin.P1))
-    moistness = pins.analogReadPin(AnalogPin.P1)
-    if (true) {
-    	
-    } else if (false) {
-    	
-    } else if (false) {
-    	
-    } else {
-    	
-    }
-})
+let onoff = 0
+let NO = false
 input.onButtonPressed(Button.AB, function () {
+    moistness = pins.analogReadPin(AnalogPin.P1)
     basic.showString("" + (moistness))
+    basic.pause(2000)
+    basic.clearScreen()
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
+    onoff += 1
+    basic.clearScreen()
+})
+input.onGesture(Gesture.Shake, function () {
+    if (NO == false) {
+        NO = true
+    } else {
+        NO = false
+    }
+})
+basic.forever(function () {
+    if (onoff >= 2) {
+        onoff = 0
+        basic.clearScreen()
+    }
+    while (onoff == 1) {
+        moistness = pins.analogReadPin(AnalogPin.P1)
+        if (moistness <= 200) {
+            basic.showLeds(`
+                . # . # .
+                . # . # .
+                . . . . .
+                . # # # .
+                # . . . #
+                `)
+        } else if (moistness >= 200) {
+            if (moistness <= 300) {
+                basic.showLeds(`
+                    . # . # .
+                    . # . # .
+                    . . . . .
+                    # # # # #
+                    . . . . .
+                    `)
+            } else {
+                basic.showLeds(`
+                    . # . # .
+                    . # . # .
+                    . . . . .
+                    # . . . #
+                    . # # # .
+                    `)
+            }
+        }
+    }
+    moistness = pins.analogReadPin(AnalogPin.P1)
 })
